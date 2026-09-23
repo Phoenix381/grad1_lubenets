138050c773811c8b12013faaa368a80572e00142
Lmp-off <piv.lip@mail.ru>
2026-09-23 08:13:40 +0300

    LR2 + example input for LR1

---
+++ lr2/main.cpp
@@ -0,0 +1,232 @@
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣴⣶⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠟⠉⠉⠙⠻⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢀⣴⠟⠋⠀⠀⠀⠙⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠃⠀⣠⡤⢄⡀⠀⠙⢷⣄⢀⣀⣀⣀⣾⢦⡿⣡⡶⣂⣀⣀⣴⠟⠁⠀⣠⠖⢋⠶⡀⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⠀⢰⠇⣽⣀⡻⣦⠀⠈⠛⠛⠋⠉⠉⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⢼⣭⣞⣇⣴⡇⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⢸⡃⢠⢷⡱⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢽⣴⡇⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⠀⠘⢶⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣌⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡆⠀⠈⠓⣄⠀⠀⠀⠀⡴⠁⠀⠀⠖⠒⠒⠢⢤⡀⠀⠙⣿⡛⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⣿⠇⠀⠀⠀⢀⡠⠎⠀⠀⠀⢀⣀⣀⣀⡀⠀⠘⣶⠾⠿⣶⡇⠀⣀⣤⣤⣄⣀⡀⠀⠙⠲⡄⢹⣟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡟⠀⠀⠀⡤⠋⠀⠀⣠⡴⠚⠏⠉⠉⠙⠉⠢⣤⡇⠀⠀⠈⢧⡾⠋⠁⠀⡀⠀⠋⢷⣄⠀⠘⢦⢻⣆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⠁⢀⡤⠊⠀⢀⣴⠾⠋⠀⠀⢀⣤⣤⣄⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⣠⣶⣶⡄⠀⠀⠛⢿⣄⠀⠑⢽⣧⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⠟⠔⠒⠉⠀⠀⣰⠟⠁⠀⠀⠀⠀⣾⣿⣿⣿⡆⣀⣤⣤⡄⠀⡀⣤⣤⣶⣿⣿⣿⣿⠀⠀⠀⠀⠻⣧⠀⠀⢨⣍⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠃⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠙⠿⠟⣁⠞⠉⠀⠀⣿⣿⣿⣿⡆⠀⠙⣿⠋⠁⠀⠀⠀⠀⠀⢻⡇⠀⠀⣽⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡄⠀⠀⠀⠀⢻⣇⠀⠀⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⠀⠉⠛⣿⠋⠀⠀⣀⠈⠻⢄⡀⠀⠀⢀⡰⠏⠀⠀⢠⡿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⠀⠀⠉⠳⠤⢤⣤⡤⠤⠚⠁⠀⠀⣸⣷⣦⣤⣴⠾⠻⠶⠶⠾⠛⠷⣦⡀⠀⠉⠉⠁⠀⠀⠀⣠⡿⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠟⠁⣀⣀⠀⠀⠀⢶⠄⠀⢀⣤⡌⠻⣧⡀⠀⠀⢀⣠⡾⠟⠁⠀⠀⠀⠀⠀
+⠀⠀⣤⣴⣾⠿⠒⠳⢶⣤⡀⠀⠀⢨⣿⠛⠦⠤⠀⠀⠀⠀⠀⠀⠀⠀⣾⠃⠀⠀⠿⠿⠀⠀⠀⠀⠀⠀⠈⠛⠃⢀⣸⣧⣀⠰⠛⣿⠗⠀⠀⠀⠀⠀⠀⠀
+⠀⣴⡟⠉⠀⠀⠀⢀⠀⠉⢿⣄⠀⠐⣿⠂⠀⠀⠀⠀⠀⣀⣀⣀⣤⡾⠟⣛⣻⣿⣄⠀⠀⠀⠇⠀⠀⣠⣄⠀⢠⡿⠿⡯⠻⢷⣄⣙⢷⡄⠀⠀⠀⠀⠀⠀
+⢸⣏⠃⠀⠀⠀⣠⠖⠶⠞⠋⣿⣀⣾⠏⠀⠀⠀⠀⠀⠈⠉⠉⢻⡋⠀⠀⠈⢩⣴⣿⣄⣴⣤⠀⠀⠀⠟⠛⠀⣿⡿⣶⠀⠀⠀⡿⠉⠈⢻⡆⠀⠀⠀⠀⠀
+⣿⡯⠴⠋⠉⠉⠁⠀⠀⢀⣀⢸⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆⠀⠀⠀⠀⣠⡿⠋⠛⠋⠀⠀⠀⢀⣀⠀⠛⣷⡔⠀⠀⣠⠟⠀⠀⢸⣿⠀⠀⠀⠀⠀
+⣿⡇⠀⠀⡀⣠⠖⠒⠖⠋⢩⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣆⡀⠀⣼⡟⠀⠀⠀⠀⣦⠀⠀⠿⠿⠀⢠⣾⣷⡶⠿⠋⠀⠀⠀⣸⡟⠀⠀⠀⠀⠀
+⣿⣇⣴⠋⠉⠉⠀⠀⠄⠀⣼⠇⠀⠀⠀⠀⢠⣄⠀⠀⠀⠀⠀⠀⠀⢀⣨⣿⠋⠛⢷⣤⣤⣀⣀⣀⣀⣠⣤⡿⠛⠀⠘⠻⣦⣤⣀⣤⣴⠿⣷⣄⠀⠀⠀⠀
+⣿⣷⠀⠀⠀⢀⣀⡰⠛⠙⣿⠀⠀⠀⠀⠀⠈⠙⠷⠶⢶⣤⣶⣶⠾⠟⠉⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠹⠉⠉⠀⠀⠀⠻⣧⠀⠀⠀
+⢸⣇⠀⣀⣴⠉⠁⠀⠀⢸⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠓⠀⠠⡇⠀⠀⠀⠀⠀⢹⡇⠀⠀
+⠈⢿⡏⠀⠀⠀⠀⣀⣾⡙⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠁⠀⠀⠀⠀⠀⢸⡇⠀⠀
+⠀⠈⢻⣆⠀⠀⡞⠃⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠎⠀⠀⢀⣠⣤⣤⣿⣁⠀⠀
+⠀⠀⠀⠙⢷⣴⣇⠀⠀⢀⡟⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⠶⠛⠿⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⢀⣴⠿⠋⠁⢴⣗⢻⣿⣷⡀
+⠀⠀⠀⠀⠀⠉⠻⠶⣶⣿⣀⣈⣿⣦⡀⠀⠀⠀⠀⢀⡴⠋⠁⠀⢰⣤⡘⣦⣹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣶⣯⣄⣠⣿⡀⠀⠀⠀⠀⢙⣷⣿⡿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠛⠷⣦⣤⣄⣸⣃⠀⠀⠀⠀⣸⣧⣼⣿⠟⠓⠶⠶⠶⠾⠛⠛⠛⠛⠉⠉⠀⠀⠉⠙⠛⠛⠛⠛⠛⠛⠛⠛⠉⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠙⠛⠛⠛⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <numeric>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+
+//#define DEBUG
+
+using namespace std;
+
+void print_matrix(const vector<vector<int>> &vctr){
+    for(auto r:vctr){
+        for(auto n:r){
+            cout<<n<<" ";
+        }
+        cout<<'\n';
+    }
+}
+
+bool is_digit(char ch) {
+    return ch <= 57 && ch >= 48;
+}
+
+void read_matrix(vector<vector<int>> *vctr) {
+    vctr->clear();
+
+    string s;
+    while (getline(cin, s)) {
+        if (s.find_first_not_of(" \t\r") == string::npos) {
+            if (!vctr->empty()) return;
+            else continue;
+        }
+        
+        if (!s.empty() && s[0] == ':') continue;
+
+        (*vctr).push_back(vector<int>());
+
+        vector<char> curr;
+        bool is_dev = false;
+
+        for (int i = 0; i < (int)s.size(); i++)
+        {
+            if (is_digit(s[i]) || s[i] == '.') curr.push_back(s[i]);
+
+            if (s[i] == '/') {
+                (*vctr).back().push_back(stoi(string(curr.begin(), curr.end())));
+                is_dev = true;
+                curr.clear();
+                continue;
+            }
+
+            if (s[i] == ' ' || i == (int)s.size() - 1) {
+                if (curr.empty()) { is_dev = false; continue; }
+                if (is_dev) {
+                    is_dev = false;
+                    (*vctr).back().back() /= stoi(string(curr.begin(), curr.end()));
+                }
+                else {
+                    (*vctr).back().push_back(stoi(string(curr.begin(), curr.end())));
+                }
+                curr.clear();
+            }
+        }
+    }
+}
+
+vector<int> D(const vector<vector<int>>& matrix) {
+    size_t rows = matrix.size();
+    size_t cols = matrix[0].size();
+
+    vector<int> res(cols);
+    for (size_t j = 0; j < cols; j++) {
+        int col_max = matrix[0][j];
+        for (size_t i = 1; i < rows; i++)
+            if (matrix[i][j] > col_max) col_max = matrix[i][j];
+        res[j] = col_max;
+    }
+    return res;
+}
+
+vector<vector<int>> calc_R(const vector<vector<int>>& matrix) {
+    vector<int> d = D(matrix);
+    vector<vector<int>> res = matrix;
+    for (int i = 0; i < res[0].size(); i++)
+    {
+        for (int j = 0; j < res.size(); j++)
+        {
+            res[j][i] = d[i]-res[j][i];
+        }
+    }
+    return res;
+}
+
+pair<int,double> find_max(const vector<double>& w){
+    auto el = max_element(w.begin(),w.end());
+    return {(el - w.begin()) + 1, *el};
+}
+
+pair<int,double> вальда(const vector<vector<int>>& matrix){
+    pair<int,double> ans = {1,*min_element(matrix[0].begin(),matrix[0].end())};
+    for (int i = 0; i < matrix.size(); i++){
+        double el = *min_element(matrix[i].begin(),matrix[i].end());
+        if (ans.second<el)ans = {i+1,el};
+    }
+    return ans;
+}
+
+pair<int,double> сэвиджа(const vector<vector<int>>& matrix){
+    vector<vector<int>> R = calc_R(matrix);
+    pair<int,double> ans = {1,*max_element(R[0].begin(),R[0].end())};
+    for (int i = 0; i < R.size(); i++){
+        double el = *max_element(R[i].begin(),R[i].end());
+        if (ans.second>el)ans = {i+1,el};
+    }
+    return ans;
+}
+
+// [0,m) -> M [m,2m) -> N
+pair<int,double> гурвиц(const vector<vector<int>>& matrix){
+    vector<int> ans(2*matrix.size());
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        if (i<matrix.size())ans[i] = *min_element(matrix[i].begin(),matrix[i].end());
+        else ans[i] = *max_element(matrix[i-matrix.size()].begin(),matrix[i-matrix.size()].end());
+    }
+    vector<double> w(matrix.size());
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        w[i] = (ans[i] + ans[i+matrix.size()])/2.0;
+    }
+    return find_max(w);
+}
+
+pair<int,double> лаплас(const vector<vector<int>>& matrix){
+    vector<double> w(matrix.size());
+    for (int i = 0; i < w.size(); i++)
+    {
+        w[i] = accumulate(matrix[i].begin(),matrix[i].end(),0.0)/matrix[0].size();
+    }
+    return find_max(w);
+}
+
+pair<int,double> ходж(const vector<vector<int>>& matrix){
+    vector<double> w(matrix.size());
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        w[i] = (accumulate(matrix[i].begin(),matrix[i].end(),0.0)/matrix[0].size() + *min_element(matrix[i].begin(),matrix[i].end()))/2;
+    }
+
+    return find_max(w);
+}
+
+void solve() {
+    freopen("input.txt", "r", stdin);
+    cout.precision(10);
+    
+    vector<vector<int>> A;
+    
+    read_matrix(&A);
+    print_matrix(A);
+    
+    freopen("output.txt", "w", stdout);
+    
+    pair<int,double> q1 = вальда(A);
+    cout<<"по критерию Вальда: "<<q1.second<<" при стратегии "<<q1.first<<'\n';
+    
+    pair<int,double> q2 = сэвиджа(A);
+    cout<<"по критерию Сэвиджа: "<<q2.second<<" при стратегии "<<q2.first<<'\n';
+    
+    pair<int,double> q3 = гурвиц(A);
+    cout<<"по критерию Гурвица: "<<q3.second<<" при стратегии "<<q3.first<<'\n';
+    
+    pair<int,double> q4 = лаплас(A);
+    cout<<"по критерию Байеса: "<<q4.second<<" при стратегии "<<q4.first<<'\n';
+    
+    pair<int,double> q5 = ходж(A);
+    cout<<"по критерию Ходжа: "<<q5.second<<" при стратегии "<<q5.first<<'\n';
+}
+
+#ifdef DEBUG
+#define HELLO cout << "******DEBUG******\n";
+void debug() {
+    HELLO;
+}
+#endif
+
+signed main(){
+    solve();
+    
+#ifdef DEBUG
+    debug();
+#endif // !DEBUG
+
+    system("pause");
+    return 0;
+}
